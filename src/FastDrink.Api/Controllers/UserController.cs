febb03065c91e1b845e0873668e70834e71c7b68
Chazita <chazarreta.matias.1138@gmail.com>
2022-03-30 19:01:12 -0300

    address

---
+++ src/FastDrink.Api/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using FastDrink.Application.Addresses.Commands;
+using FastDrink.Application.Users.Commands;
+using FastDrink.Application.Users.DTOs;
+using HashidsNet;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastDrink.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IHashids _hashids;
+
+    public UserController(IMediator mediator, IHashids hashids)
+    {
+        _mediator = mediator;
+        _hashids = hashids;
+    }
+
+    [HttpPost("add-prefered-address")]
+    [Authorize(Policy = "MustBeUser")]
+    public async Task<ActionResult> AddPreferedAddress(CreateAddressCommand request)
+    {
+        var userData = UserClaims.GetUser(User);
+
+        var addressResult = await _mediator.Send(request);
+
+        if (!addressResult.Succeeded)
+        {
+            return BadRequest(addressResult.Errors);
+        }
+
+        var result = await _mediator.Send(new AddPreferedAddressCommand
+        {
+            AddressId = addressResult.AddressId,
+            UserId = _hashids.Decode(userData.Id)[0]
+        });
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return NoContent();
+    }
+}
