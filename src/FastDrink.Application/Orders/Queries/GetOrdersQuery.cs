9cd2c08b3a89be7604c963b5afca517df8321f45
Chazita <chazarreta.matias.1138@gmail.com>
2022-07-10 02:31:11 -0300

    add sort

--- src/FastDrink.Application/Orders/Queries/GetOrdersQuery.cs
+++ src/FastDrink.Application/Orders/Queries/GetOrdersQuery.cs
@@ -1,35 +1,55 @@
 ﻿using AutoMapper;
+using HashidsNet;
 using AutoMapper.QueryableExtensions;
 using FastDrink.Application.Common.Interfaces;
 using FastDrink.Application.Common.Mappings;
 using FastDrink.Application.Common.Models;
 using FastDrink.Application.Orders.DTOs;
 using MediatR;
+using FastDrink.Domain.Enums;
 
 namespace FastDrink.Application.Orders.Queries;
 
 public class GetOrdersQuery : IRequest<PaginatedList<OrderAdminDto>>
 {
     public int PageNumber { get; set; } = 1;
-
     public int PageSize { get; set; } = 10;
+    public string Search { get; set; } = string.Empty;
+    public string OrderByStatus { get; set; } = string.Empty;
 }
 
 public class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, PaginatedList<OrderAdminDto>>
 {
     private readonly IApplicationDbContext _context;
-    public readonly IMapper _mapper;
+    private readonly IMapper _mapper;
+    private readonly IHashids _hashids;
 
-    public GetOrdersQueryHandler(IMapper mapper, IApplicationDbContext context)
+    public GetOrdersQueryHandler(IMapper mapper, IApplicationDbContext context, IHashids hashids)
     {
         _mapper = mapper;
         _context = context;
+        _hashids = hashids;
     }
 
     public async Task<PaginatedList<OrderAdminDto>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
     {
-        return await _context.Order
+        var orderQuery = _context.Order.AsQueryable();
+
+        if(request.Search.Length > 0 && request.Search[0] != '#'){
+            int id = _hashids.Decode(request.Search)[0];
+
+            orderQuery = orderQuery.Where(x => x.Id == id).AsQueryable();
+        }
+
+        if(request.OrderByStatus.Length > 0){
+            var status = (OrderStatus)Enum.Parse(typeof(OrderStatus),request.OrderByStatus);
+            
+            orderQuery = orderQuery.Where(x => x.OrderStatus == status).AsQueryable();
+        }
+
+        return await orderQuery
             .ProjectTo<OrderAdminDto>(_mapper.ConfigurationProvider)
+            .OrderByDescending(x => x.LastModified)
             .PaginatedListAsync(request.PageNumber, request.PageSize);
     }
 }
